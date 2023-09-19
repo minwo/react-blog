d44a322c2e44d1c8e0024324700f7bd0e2d46b70
minwo <cossn81@gmail.com>
2023-09-19 17:33:48 +0900

    dg
    sdg

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 function App() {
   let post = "강남 우동 맛집";
   let [글제목, b] = useState(['여자 코트 추천', 'ReactBlog', '네이버 블로그']);
-  let [따봉, 따봉변경] = useState([0,0,0]);
+  let [따봉, 따봉변경] = useState([0,10,0]);
   let [modal, setModal] = useState(false);
 
 
@@ -15,10 +15,8 @@ function App() {
   function modalFn(){
 	if(modal){
 		setModal(false);
-		console.log(modal);
 	} else {
 		setModal(true);
-		console.log(modal);
 	}
   }
 
@@ -26,19 +24,19 @@ function App() {
     <div className="App">
 		{
 			글제목.map(function(a, i){
-				console.log(따봉, typeof(따봉));
 				return(
 					<div className="list" key={i}>
-						<h4>
+						<h4 onClick={(e) =>{modalFn();handler(e)}} data-index={i}>
 							{글제목[i]}
-							<span onClick={() => {
+							<span onClick={() => { 
 								let count = [...따봉];
 								count[i] = count[i] + 1;
-								따봉변경(count)
-							}}>👍</span>
+								따봉변경(count)}
+							}>👍</span>
 							{따봉[i]}
 						</h4>
 						<p>2월17일 발행</p>
+						<button>글수정</button>
 					</div>
 				)	
 			})
@@ -46,16 +44,17 @@ function App() {
 		
 
 		{
-			modal == true? <Modal/> : null
+			modal == true? <Modal 글제목={글제목} 함수 ={handler}/> : null
 		}
     </div>
   );
 }
 
-function Modal () {
+function Modal (props,i) {
+	console.log(i);
 	return (
 		<div className="modal">
-			<h4>제목</h4>
+			<h4>{props.글제목}</h4>
 			<p>날짜</p>
 			<p>상세내용</p>
 		</div>
