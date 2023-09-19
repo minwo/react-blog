2ecd25ddfd6fefe8e9a425cf57024c84ae3da2ef
minwo <cossn81@gmail.com>
2023-09-19 12:55:39 +0900

    dg
    dg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let post = "강남 우동 맛집";
+  let [글제목, b] = useState('여자 코트 추천');
+  let [logo, setLogo] = useState(['ReactBlog', '네이버 블로그']);
+  let [따봉, 따봉변경] = useState(0);
+
+  function 함수(){
+	따봉변경(따봉 + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="list">
+		<h4>{ 글제목 } <span onClick={ 함수 }>👍</span> {따봉} </h4>
+		<p>2월 17일 발행</p>
+	  </div>
+      <div className="list">
+		<h4>{ logo[0] }</h4>
+		<p>2월 17일 발행</p>
+	  </div>
+      <div className="list">
+		<h4>{ logo[1] }</h4>
+		<p>2월 17일 발행</p>
+	  </div>
     </div>
   );
 }
